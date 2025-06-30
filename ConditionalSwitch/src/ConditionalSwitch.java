16254475f19a0e8363f7af1ffaa320d97cd9aefa
anishasuman <anisha77suman1980@gmail.com>
2025-06-30 16:43:24 +0530

    Added Conditional Switch in Java

---
+++ ConditionalSwitch/src/ConditionalSwitch.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+//public class ConditionalSwitch{
+//    public static void main(String[] args){
+//        //Conditional Statement - SWITCH
+//week days
+//        int day = 2 ; // 1- mon , 2 - Tue , 3 - Wed etc.
+//        switch(day){
+//            case 1 :
+//                System.out.println("monday");
+//                break;
+//            case 2:
+//                System.out.println("tuesday");
+//                break;
+//            case 3:
+//                System.out.println("wed-sun");
+//        }
+//    }
+//}
+//perform basic arithmetic operations
+public class ConditionalSwitch{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first No: ");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter Second No: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Enter Operator like (+ , - , / , *) : ");
+        char op = sc.next().charAt(0);
+        double result;
+        switch (op){
+            case '+' :
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if(num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by Zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator");
+        }
+    }
+}
+
