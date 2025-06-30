4e99c425e32ce8d33d314e77f1a085d24b38a5f4
anishasuman <anisha77suman1980@gmail.com>
2025-06-30 17:23:21 +0530

    Added more code to vowel checker in java

--- ConditionalSwitch/src/ConditionalSwitch.java
+++ ConditionalSwitch/src/ConditionalSwitch.java
@@ -17,40 +17,67 @@ import java.util.Scanner;
 //    }
 //}
 //perform basic arithmetic operations
-public class ConditionalSwitch{
-    public static void main(String[] args){
+//public class ConditionalSwitch{
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter first No: ");
+//        double num1 = sc.nextDouble();
+//        System.out.println("Enter Second No: ");
+//        double num2 = sc.nextDouble();
+//        System.out.println("Enter Operator like (+ , - , / , *) : ");
+//        char op = sc.next().charAt(0);
+//        double result;
+//        switch (op){
+//            case '+' :
+//                result = num1 + num2;
+//                System.out.println("Result: " + result);
+//                break;
+//            case '-':
+//                result = num1 - num2;
+//                System.out.println("Result: " + result);
+//                break;
+//            case '*':
+//                result = num1 * num2;
+//                System.out.println("Result: " + result);
+//                break;
+//            case '/':
+//                if(num2 != 0) {
+//                    result = num1 / num2;
+//                    System.out.println("Result: " + result);
+//                } else {
+//                    System.out.println("Error: Division by Zero");
+//                }
+//                break;
+//            default:
+//                System.out.println("Invalid Operator");
+//        }
+//    }
+//}
+//
+import java.util.Scanner;
+
+public class ConditionalSwitch {
+    public static void main(String[] args) {
+        //Vowel Checker
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first No: ");
-        double num1 = sc.nextDouble();
-        System.out.println("Enter Second No: ");
-        double num2 = sc.nextDouble();
-        System.out.println("Enter Operator like (+ , - , / , *) : ");
-        char op = sc.next().charAt(0);
-        double result;
-        switch (op){
-            case '+' :
-                result = num1 + num2;
-                System.out.println("Result: " + result);
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.println("Result: " + result);
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.println("Result: " + result);
-                break;
-            case '/':
-                if(num2 != 0) {
-                    result = num1 / num2;
-                    System.out.println("Result: " + result);
-                } else {
-                    System.out.println("Error: Division by Zero");
-                }
+        System.out.print("Enter alphabet Letter : ");
+        char ch = sc.next().charAt(0);
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("It's a vowel.");
                 break;
             default:
-                System.out.println("Invalid Operator");
+                if (Character.isLetter(ch))
+                    System.out.println("It's a consonant.");
+                else
+                    System.out.println("Invalid input. Please enter an alphabet.");
         }
     }
 }
+
 
