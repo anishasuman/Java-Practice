06fd1aa97d998f369aae8e4ddbb039e418548ad8
anishasuman <anisha77suman1980@gmail.com>
2025-07-01 00:41:14 +0530

    Added For Loop in java

--- ConditionalSwitch/Loops.java/src/Main.java
+++ ConditionalSwitch/Loops.java/src/Main.java
@@ -1,7 +1,6 @@
 public class Loops{
     public static void main(String[] args){
-//1.Pattern Printing of Star
-        for(int i = 1 ; i <= 5 ; i++){
+        for(int i = 1 ; i < = 5 ; i++){
             for(int j = 1 ; j <= i ; j++){
                 System.out.println("*");
             }
@@ -9,3 +8,4 @@ public class Loops{
         }
     }
 }
+
