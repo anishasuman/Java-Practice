6d88a6f1ae7b87c499a3b2772dfae3ecbad76089
anishasuman <anisha77suman1980@gmail.com>
2025-07-01 13:56:18 +0530

    Day 2: Added While Loop in Java

---
+++ WhileLoop.java
@@ -0,0 +1,34 @@
+//public class Main {
+//1.Pattern Printing
+//    public static void main(String[] args) {
+//        int i = 1;
+//        while (i <= 5) {
+//            int j = 1;
+//            while (j <= i) {
+//                System.out.print("*");
+//                j++;
+//            }
+//            System.out.println();
+//            i++;
+//        }
+//    }
+//}
+//2.Inverted Triangle
+public class Main {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i >= 1) {
+            int j = 1;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i--;
+        }
+    }
+}
+
+
+
+
