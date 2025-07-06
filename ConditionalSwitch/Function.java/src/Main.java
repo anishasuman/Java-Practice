f4401b71ed62b471ff38c6ea8a64ad3045b58c68
anishasuman <anisha77suman1980@gmail.com>
2025-07-06 14:19:45 +0530

    Added Function in java

---
+++ ConditionalSwitch/Function.java/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+
+//public class Main{
+//    public static void printJava(){
+//        System.out.println("Hello Everyone!!");
+//    }
+//    public static void printName(String name){
+//        System.out.println(name);
+//    }
+//    public static void main(String[] args){
+//        //METHOD
+//        //printJava("Hello Everyone!!");
+//        printName("Anisha:)");
+//    }
+//}
+public class Main{
+    public static void printSum(int a , int b){
+        //int a = 2 , b = 3 ;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    public static void main(String[] args){
+        //Method
+        printSum(2,3);
+    }
+}
---
+++ Exception.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        //TRY&CATCH in Exception Handling
+        int[] marks = {97,98,95};
+        try{
+            System.out.println(marks[5]);
+        }catch(Exception exception){
+            //do something after catching
+        }
+
+        System.out.println("The name of student is Aman");
+
+    }
+}
