636347965ef3ecb292c86d2277517b792cfd5815
anishasuman <anisha77suman1980@gmail.com>
2025-07-06 15:21:58 +0530

    Added Non-Parameterized Constructor in Java

---
+++ Object.java
@@ -0,0 +1,22 @@
+class Student{
+    String name;
+    int age;
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+    //Non-parameterized Constructor . 
+    // yeh by default saved rahta h java me !! No need to write. 
+    Student(){
+        System.out.println("Constructor Called");
+    }
+}
+public class Object {
+    public static void main(String[] args){
+        Student s1 = new Student();
+        s1.name = "Anisha";
+        s1.age = 23;
+
+        s1.printInfo();
+    }
+}
