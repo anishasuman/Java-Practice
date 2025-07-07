28a3fb5696f97c172edd2330909337d418de64d1
anishasuman <anisha77suman1980@gmail.com>
2025-07-07 20:39:07 +0530

    Added Paramterized Constructor in Java.

--- Directory of Java Topics/Function.java/src/Object.java
+++ Directory of Java Topics/Function.java/src/Object.java
@@ -1,3 +1,25 @@
+//class Student{
+//    String name;
+//    int age;
+//    public void printInfo(){
+//        System.out.println(this.name);
+//        System.out.println(this.age);
+//    }
+//    //Non-parameterized Constructor .
+//    // yeh by default saved rahta h java me !! No need to write.
+//    Student(){
+//        System.out.println("Constructor Called");
+//    }
+//}
+//public class Object {
+//    public static void main(String[] args){
+//        Student s1 = new Student();
+//        s1.name = "Anisha";
+//        s1.age = 23;
+//
+//        s1.printInfo();
+//    }
+//}
 class Student{
     String name;
     int age;
@@ -5,18 +27,22 @@ class Student{
         System.out.println(this.name);
         System.out.println(this.age);
     }
-    //Non-parameterized Constructor .
-    // yeh by default saved rahta h java me !! No need to write.
+    //Parameterized Constructor
+    Student(Student s2){
+        this.name = s2.name;
+        this.age = s2.age;
+    }
     Student(){
-        System.out.println("Constructor Called");
+
     }
 }
-public class Object {
+public class Object{
     public static void main(String[] args){
         Student s1 = new Student();
         s1.name = "Anisha";
         s1.age = 23;
+        Student s2 = new Student(s1);
+        s2.printInfo();
 
-        s1.printInfo();
     }
 }
